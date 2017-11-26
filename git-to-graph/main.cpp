9e2690cab0b25ebd82530bf5d0edaa331066bc57
BrianKovacs <brian.kovacs@me.com>
2017-11-26 02:20:06 -0500

    Initial Commit

---
+++ git-to-graph/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  git-to-graph
+//
+//  Created by Brian Kovacs on 11/26/17.
+//  Copyright © 2017 Brian Kovacs. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
