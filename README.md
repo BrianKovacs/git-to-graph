fa65e992b780d3913a6cbfd553d604b03158a7e6
BrianKovacs <brian.kovacs@me.com>
2017-11-26 03:14:23 -0500

    Push to remote

---
+++ README.md
@@ -0,0 +1,2 @@
+# git-to-graph
+Generate a GEXF graph from a git repository
--- git-to-graph/main.cpp
+++ git-to-graph/main.cpp
@@ -3,13 +3,47 @@
 //  git-to-graph
 //
 //  Created by Brian Kovacs on 11/26/17.
-//  Copyright © 2017 Brian Kovacs. All rights reserved.
 //
 
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+string GetStdoutFromCommand(string cmd) {
+    
+    string data;
+    FILE * stream;
+    const int max_buffer = 256;
+    char buffer[max_buffer];
+    cmd.append(" 2>&1");
+    
+    stream = popen(cmd.c_str(), "r");
+    if (stream) {
+        while (!feof(stream))
+            if (fgets(buffer, max_buffer, stream) != NULL) data.append(buffer);
+        pclose(stream);
+    }
+    return data;
+}
 
 int main(int argc, const char * argv[]) {
+    
     // insert code here...
-    std::cout << "Hello, World!\n";
+    string cmd;
+    if (argc == 1)
+        cmd = "git log --pretty=format:'%h - %ae' --name-status --reverse";
+    else if (argc == 2)
+        cmd = "git -C " + string(argv[1]) + " log --pretty=format:'%h - %ae' --name-status --reverse";
+    else {
+        cout << "Improper arguments.";
+        return 0;
+    }
+    
+    //    string ls = GetStdoutFromCommand("ls -la");
+    string log = GetStdoutFromCommand(cmd);
+    cout << "git log:\n" << log << endl;
+    
     return 0;
 }
