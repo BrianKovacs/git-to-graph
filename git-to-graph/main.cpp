b991e95d5984f591b6ad0943d4ae317b29763532
BrianKovacs <brian.kovacs@me.com>
2017-11-26 20:27:59 -0500

    Nodes and edges added to separate maps

--- git-to-graph/main.cpp
+++ git-to-graph/main.cpp
@@ -20,11 +20,12 @@ typedef map<string, int> EdgeMap;
 
 bool isHash(const string &str);
 void addNode(const string &file, const string &status, int commit, NodeMap &nodes);
-void createEdges(const vector<string> &edgeGroup, int commit);
-void addEdge(const string &edge);
+void createEdges(const vector<string> &edgeGroup, int commit, EdgeMap &edges);
+void addEdge(const string &edge, EdgeMap &edges);
 
 int main(int argc, const char * argv[])
 {
+    cout << "Start\n";
     // Validate arguments
     string cmd;
     if (argc == 1)
@@ -65,7 +66,7 @@ int main(int argc, const char * argv[])
         // Check if first line starts with hash (so this is a new commit)
         if (isHash(line)) {
             // Create edges from the previous commit
-            createEdges(edgeGroup, c);
+            createEdges(edgeGroup, c, edges);
             // Empty the edge group
             edgeGroup.clear();
             // Advance the commit number
@@ -75,28 +76,42 @@ int main(int argc, const char * argv[])
             // Separate file and status
             file = line.substr(line.find_first_of('\t')+1);
             status = line.substr(0, line.find_first_of('\t'));
-            // Add file to node map
-            addNode(file, status, c, nodes);
-            // Add file to current enge group
-            edgeGroup.push_back(file);
-            // Advance the commit number
-            ++f;
+            // If status is not rename...
+            if (status[0] != 'R') {
+                // Add file to node map
+                addNode(file, status, c, nodes);
+                // Add file to current enge group
+                edgeGroup.push_back(file);
+                // Advance the commit number
+                ++f;
+            }
         }
     }
     
-    /*
     cout << "Commits:\t" << c << endl << "Files:\t" << f << endl;
-    NodeMap::iterator itr;
+    
+    /*
+    NodeMap::iterator i;
     cout << "\nNode Map: \n";
     cout << "\tDEG\tFILE\n";
-    for (itr = nodes.begin(); itr != nodes.end(); ++itr)
+    for (i = nodes.begin(); i != nodes.end(); ++i)
     {
-        cout << '\t' << itr->second
-        << '\t' << itr->first << '\n';
+        cout << '\t' << i->second
+        << '\t' << i->first << '\n';
     }
     cout << endl;
-     */
     
+    EdgeMap::iterator j;
+    cout << "\nEdge Map: \n";
+    cout << "\tW\tEDGE\n";
+    for (j = edges.begin(); j != edges.end(); ++j)
+    {
+        cout << '\t' << j->second
+        << '\t' << j->first << '\n';
+    }
+    cout << endl;
+    */
+    cout << "End\n";
     return 0;
 }
 
@@ -107,35 +122,52 @@ bool isHash(const string &s)
 
 void addNode(const string &file, const string &status, int commit, NodeMap &nodes)
 {
-    if (status[0] == 'R') {
-        // File was renamed
-        // ...
+    NodeMap::iterator lb = nodes.lower_bound(file);
+    if(lb != nodes.end() && !(nodes.key_comp()(file, lb->first)))
+    {
+        // key already exists
+        // update lb->second
+        lb->second++;
     } else {
-        NodeMap::iterator lb = nodes.lower_bound(file);
-        if(lb != nodes.end() && !(nodes.key_comp()(file, lb->first)))
-        {
-            // key already exists
-            // update lb->second
-            lb->second++;
-        } else {
-            // the key does not exist in the map
-            // Use lb as a hint to insert, so it can avoid another lookup
-            nodes.insert(lb, NodeMap::value_type(file, 1));
+        // the key does not exist in the map
+        // Use lb as a hint to insert, so it can avoid another lookup
+        nodes.insert(lb, NodeMap::value_type(file, 1));
+    }
+}
+
+void createEdges(const vector<string> &edgeGroup, int commit, EdgeMap &edges)
+{
+    /*
+    cout << "Commit " << commit << endl;
+    vector<string>::const_iterator t;
+    for(t=edgeGroup.begin(); t!=edgeGroup.end(); ++t){
+        cout << '\t' << (*t) << endl;
+    }
+    */
+    string edge;
+    vector<string>::const_iterator i;
+    vector<string>::const_iterator j;
+    for(i=edgeGroup.begin(); i!=edgeGroup.end(); ++i){
+        for(j=i; j!=edgeGroup.end(); ++j){
+//        cout << "\t\t" << (*i) << "<--->" << (*j) << endl;
+            edge = (*i) + ":" + (*j);
+            addEdge(edge, edges);
         }
     }
 }
 
-void createEdges(const vector<string> &edgeGroup, int commit)
+void addEdge(const string &edge, EdgeMap &edges)
 {
-    cout << "Commit " << commit << endl;
-    vector<string>::const_iterator i;
-    for(i=edgeGroup.begin(); i!=edgeGroup.end(); ++i){
-        cout << '\t' << (*i) << endl;
+    EdgeMap::iterator lb = edges.lower_bound(edge);
+    if(lb != edges.end() && !(edges.key_comp()(edge, lb->first)))
+    {
+        // key already exists
+        // update lb->second
+        lb->second++;
+    } else {
+        // the key does not exist in the map
+        // Use lb as a hint to insert, so it can avoid another lookup
+        edges.insert(lb, EdgeMap::value_type(edge, 1));
     }
-}
-
-void addEdge(const string &edge)
-{
-    
 }
 
